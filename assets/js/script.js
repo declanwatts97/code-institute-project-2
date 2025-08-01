6f618b59d4a8ec2cc3ef5c2e2a1abaf9bc46c7e1
declanwatts97 <declan.watts97@gmail.com>
2025-08-01 19:27:18 +0100

    added javascript for trivia game, added some style changes

---
+++ assets/js/script.js
@@ -0,0 +1,134 @@
+/**
+ * Questions to be used in the trivia quiz
+ */
+const questions = [
+    {
+        question: "What is the capital of Iceland?",
+        answers: [
+            { text: "Reykjavik", correct: true },
+            { text: "Akureyri", correct: false },
+            { text: "Keflavik", correct: false },
+            { text: "Hafnarfjordur", correct: false },
+        ]
+    },
+    {
+        question: "What is the largest glacier in Iceland, and also Europe?",
+        answers: [
+            { text: "Vatnajökull", correct: true },
+            { text: "Langjökull", correct: false },
+            { text: "Eyjafjallajökull", correct: false },
+            { text: "Mýrdalsjökull", correct: false },
+        ]
+    },
+    {
+        question: "Iceland has the world's oldest surviving parliment, when was it established?",
+        answers: [
+            { text: "930 AD", correct: true },
+            { text: "1564 AD", correct: false },
+            { text: "1675 AD", correct: false },
+            { text: "1268 AD", correct: false },
+        ]
+    },
+    {
+        question: "What is the highest mountain in Iceland?",
+        answers: [
+            { text: "Hvannadalshnúkur", correct: true },
+            { text: "Snæfell", correct: false },
+            { text: "Öræfajökull", correct: false },
+            { text: "Tindfjallajökull", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");    
+const nextButton = document.getElementById("next-button");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+/**
+ * Function to start the quiz
+ */
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+};
+
+/**
+ * Pulls the questions from the array at the top of the file and shows them on the page
+ */
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("button");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+/**
+ * Removes placeholder text from the DOM
+ */
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+/**
+ * Shows whether chosen answer is true or false and adds class based on result
+ */
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
